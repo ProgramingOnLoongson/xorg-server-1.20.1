b8f1e277d0ed283e3dcc0453cbbaf6d3a5818269
rpm-build <rpm-build>
2020-02-20 23:03:02 +0800

    0001-modesetting-Hide-atomic-behind-Option-Atomic-boolean.patch

--- hw/xfree86/drivers/modesetting/driver.c
+++ hw/xfree86/drivers/modesetting/driver.c
@@ -131,6 +131,7 @@ static const OptionInfoRec Options[] = {
     {OPTION_PAGEFLIP, "PageFlip", OPTV_BOOLEAN, {0}, FALSE},
     {OPTION_ZAPHOD_HEADS, "ZaphodHeads", OPTV_STRING, {0}, FALSE},
     {OPTION_DOUBLE_SHADOW, "DoubleShadow", OPTV_BOOLEAN, {0}, FALSE},
+    {OPTION_ATOMIC, "Atomic", OPTV_BOOLEAN, {0}, FALSE},
     {-1, NULL, OPTV_NONE, {0}, FALSE}
 };
 
@@ -1061,7 +1062,9 @@ PreInit(ScrnInfoPtr pScrn, int flags)
     }
 
     ret = drmSetClientCap(ms->fd, DRM_CLIENT_CAP_ATOMIC, 1);
-    ms->atomic_modeset = (ret == 0);
+    if ((ms->atomic_modeset = (ret == 0)))
+        ms->atomic_modeset = xf86ReturnOptValBool(ms->drmmode.Options,
+                                                  OPTION_ATOMIC, FALSE);
 
     ms->kms_has_modifiers = FALSE;
     ret = drmGetCap(ms->fd, DRM_CAP_ADDFB2_MODIFIERS, &value);
